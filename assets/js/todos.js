dddf3eee3a65451e6e83488e9013ba53ff6402b4
Paul Macfarlane <pj16macf@siena.edu>
2020-06-11 14:44:02 -0400

    added remove item functionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,4 +1,12 @@
 // toggle checked and unchecked
 $("li").on("click", function(){
-    $(this).toggleClass("checked");
+    $(this).toggleClass("completed");
+});
+
+// add delete function
+$("span").on("click", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove(); // don't just fade out, remove the element
+    });
+    event.stopPropagation(); // prevent event bubbling
 });
