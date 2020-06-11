688dbba8427f37105661a897c242e0d83a08b0c0
Paul Macfarlane <pj16macf@siena.edu>
2020-06-11 16:04:45 -0400

    app styling and animations complete

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,3 +1,12 @@
+//initialize the app with the input hidden
+function init(){
+    $("input[type='text']").hide();
+}
+
+init();
+
+// add click and keypress listeners
+
 // toggle checked and unchecked
 $("ul").on("click", "li",function(){
     $(this).toggleClass("completed");
@@ -11,11 +20,17 @@ $("ul").on("click", "span",function(event){
     event.stopPropagation(); // prevent event bubbling
 });
 
+// listener for input
 $("input[type='text']").on("keypress",function(event){
     if (event.which === 13){
         let text = $(this).val();
         $(this).val("");
         //creat new li and add to ul
-        $("ul").append("<li> <span>X</span> " + text +" </li>");
+        $("ul").append("<li> <span><i class='fa fa-trash' aria-hidden='true'></i></span> " + text +" </li>");
     }
+});
+
+// listener for adding new To Do
+$(".fa-plus").on("click", function(){
+    $("input[type='text']").fadeToggle();
 });
