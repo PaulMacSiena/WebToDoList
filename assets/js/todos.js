7e59cd9693615d696daf910ac9f86efcf7c5aac7
Paul Macfarlane <pj16macf@siena.edu>
2020-06-11 14:58:32 -0400

    finshed functionality, need to add styling

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,21 @@
 // toggle checked and unchecked
-$("li").on("click", function(){
+$("ul").on("click", "li",function(){
     $(this).toggleClass("completed");
 });
 
 // add delete function
-$("span").on("click", function(event){
+$("ul").on("click", "span",function(event){
     $(this).parent().fadeOut(500, function(){
         $(this).remove(); // don't just fade out, remove the element
     });
     event.stopPropagation(); // prevent event bubbling
+});
+
+$("input[type='text']").on("keypress",function(event){
+    if (event.which === 13){
+        let text = $(this).val();
+        $(this).val("");
+        //creat new li and add to ul
+        $("ul").append("<li> <span>X</span> " + text +" </li>");
+    }
 });
